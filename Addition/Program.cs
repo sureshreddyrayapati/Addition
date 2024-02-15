946f2f97b3c1494f0f39f2cd2b8440f3a6468a91
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-15 12:59:36 +0530

    Add project files.

---
+++ Addition/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("table of 5");
+int n = 5;
+for(int i = 1; i <= n; i++)
+{
+    Console.WriteLine("{0} * {1} = {2}",5,i,(n*i));
+}
